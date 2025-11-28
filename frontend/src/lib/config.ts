ed623c2312992946d3552269ba34aed52dc83553
Devraj Rajput <rajkirar2002.pc@gmail.com>
2025-11-29 01:38:15 +0530

    .

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,2 +1,1 @@
-// export const API_BACKEND_URL = "https://restaurant-menu-pdf-annotator.onrender.com"
-export const API_BACKEND_URL = "http://localhost:7000"
+export const API_BACKEND_URL = "https://restaurant-menu-pdf-annotator.onrender.com"
